d37b2205edc469b8d3f21ec354ca987f44656644
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-02-26 17:36:54 -0300

    putting pieces in places to start with

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,50 @@
 require_relative './pawn'
+require_relative './rook'
+require_relative './knight'
+require_relative './bishop'
 require 'paint'
 class Board
   attr_accessor :board
+
+  def self.set_board
+    b = self.new
+    (0..7).each do |position|
+      # b.display
+        piece = Pawn.new(:white)
+      b.place(piece, [position,1])
+    end
+    (0..7).each do |pos|
+      piece = Pawn.new(:black)
+      b.place(piece, [pos, 6])
+    end
+    [0,7].each do |pos|
+      piece = Rook.new(:white)
+      b.place(piece, [pos, 0])
+    end
+    [0,7].each do |pos|
+      piece = Rook.new(:black)
+      b.place(piece, [pos, 7])
+    end
+    [1,6].each do |pos|
+      piece = Knight.new(:white)
+      b.place(piece,[pos, 0])
+    end
+    [1,6].each do |pos|
+      piece = Knight.new(:black)
+      b.place(piece,[pos, 7])
+    end
+    [2,5].each do |pos|
+      piece = Bishop.new(:white)
+      b.place(piece,[pos,0])
+    end
+    [2,5].each do |pos|
+      piece = Bishop.new(:black)
+      b.place(piece,[pos,7])
+    end
+
+    b
+  end
+
   def initialize
     @board = Array.new(8){Array.new(8){}}
   end
@@ -26,27 +69,9 @@ class Board
   end
 end
 
-b = Board.new
-piec = Pawn.new(:white)
-# b.display
-(0..7).each do |position|
-  # b.display
-    piece = Pawn.new(:white)
-  b.place(piece, [position,1])
-end
-(0..7).each do |pos|
-  piece = Pawn.new(:black)
-  b.place(piece, [pos, 6])
-end
-
-# b.place('king', [1,2])
-# b.place('king', [1,-2])
-# p b.board
+b = Board.set_board
 b.display
 
 
-p b.is_out?([1,2])
-p b.is_out?([0,2])
-p b.is_out?([-1,2])
 
-puts Paint['Ruby', Paint.random]
+puts Paint[b.board.each, Paint.random]
